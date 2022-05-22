25d4eac39cbae255b0cbeb57be5519c8b85b8645
SilverLight96 <tim960321@naver.com>
2022-05-22 23:48:04 +0900

    Add 2348

--- client/src/components/common/MovieCard.vue
+++ client/src/components/common/MovieCard.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="max-w-sm bg-white w-52 rounded-lg dark:bg-gray-800 dark:border-gray-700">
     <a href="https://www.naver.com">
-        <!-- <img class="rounded-t-lg" src="https://image.tmdb.org/t/p/w300/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg" alt="" /> -->
-        <img :src="imgSrc" alt="포스터 없음">
+        <img class="rounded-t-lg" src="https://image.tmdb.org/t/p/w300/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg" alt="" />
+        <!-- <img :src="imgSrc" alt="포스터 없음"> -->
     </a>
     <div class="mt-1">
         <a href="https://www.naver.com">
             <h5 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-              <!-- 범죄도시2 -->
-              {{ movie }}
-              {{ movie.title }}
+              범죄도시2
+              <!-- {{ movie }}
+              {{ movie.title }} -->
             </h5>
         </a>
 
@@ -22,7 +22,6 @@
 
 <script>
 // import { object } from '@amcharts/amcharts5'
-// import tmdb from '@/@/fixtures/tmdb.json'
 export default {
 name: 'MovieCard',
 data(){ 
