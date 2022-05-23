78dd6781c6d5776344c3bfac6486d97a5b78c5f2
zxjk78 <zxjk78@gmail.com>
2022-05-23 20:08:05 +0900

    feat/

--- server/reviews/views.py
+++ server/reviews/views.py
@@ -43,17 +43,5 @@ def review_create(request, movie_pk):
             serializer.save()    
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-# @authentication_classes([IsAuthenticated])
-# @swagger_auto_schema(methods=['PUT'], request_body=ReviewSerializer)
-# @api_view(['PUT'])
-# def review_update_detail(request, movie_pk):
-#     user = request.user
-#     movie = Movie.objects.get(pk=movie_pk)
-#     # 이전에는 내용에 대한 저장을 안했기 때문에, 리뷰 객체를 불러와서 update해야한다.
-#     review = Review.objects.get(user=user, movie=movie) # reviewPK를 페이지에서 던져준다음 받던가, 아니면 user, movie로 조회하던가
-#     serializer =  ReviewSerializer(review, data=request.data, partial=True)
-#     # 저장 단계 전에 외래키 필드값의 부재에 오류 안나게 하려면 serializer에서 read_only 옵션이나 read_only_fields 작성
-#     if serializer.is_valid(raise_exception=True):
-#         serializer.save()    
-#     return Response(serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
+
 
