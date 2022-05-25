f00130f8293515f82569a47876158c6cc7322924
SilverLight96 <tim960321@naver.com>
2022-05-26 00:22:51 +0900

    Add 1222

--- client/src/components/common/MovieCard.vue
+++ client/src/components/common/MovieCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="max-w-sm bg-white w-52 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-    <!-- <router-link :to="{name:'home'}"> -->
-      <a :href="'http://localhost:8080/'+movie.pk+'/detail/'">
+  <div class="max-w-sm bg-white w-52 rounded-lg dark:bg-gray-800 dark:border-gray-700 scale-75">
+    <router-link :to="{name:'movie_detail', params:{movieId:movie.pk}}">
+      <!-- <a :href="'http://localhost:8080/'+movie.pk+'/detail/'"> -->
       <!-- <a href="https://www.naver.com"> -->
-        <img :src="'https://image.tmdb.org/t/p/w300/'+movie.poster_path" alt="" class="mx-auto"/>
-      </a>
-    <!-- </router-link> -->
+        <img :src="'https://image.tmdb.org/t/p/w300/'+movie.poster_path" alt="" class="mx-auto" />
+      <!-- </a> -->
+    </router-link>
     <div>
       <div class="text-2xl font-bold">{{movie.title}}</div>
       <div class="text-gray-500 text-xl font-bold">⭐{{movie.vote_average}}</div>
--- client/src/components/detail/MovieDetail.vue
+++ client/src/components/detail/MovieDetail.vue
@@ -3,17 +3,45 @@
   <!-- <template v-if="arrType==1"> -->
 
       <div class="popular-list row row-cols-1 row-cols-md-5 gy-3">
-      <div class="text-2xl font-bold">{{moviePK.title}}</div>
-      <div class="text-gray-500 text-xl font-bold">⭐{{moviePK.vote_average}}</div>
-    </div>
+      <div class="text-2xl font-bold">{{movie['title']}}</div>
+      <div class="text-gray-500 text-xl font-bold">⭐평점: {{movie['vote_average']}}</div>
+      <div class="text-gray-500 text-xl font-bold">⭐장르: {{movie['genre_ids']}}</div>
+      <div class="text-gray-500 text-xl font-bold">⭐영화 개봉일: {{movie['release_date']}}</div>
+      <div class="flex flex-wrap">
+        <div class="text-gray-500 text-xl flex-grow" v-for="actor in actors" :key="actor.pk">
+        <img :src="`https://image.tmdb.org/t/p/w500/${actor.profile_path}`" class="w-32" alt="">
+        {{actor.korean_name}}
+      </div>
+      </div>
+      
+      <!-- <div class="bg-red-100">
+        <router-link :to="{name: 'actor_detail', params:{actorId:actor.pk}}"><img :src="movie_actor.profile_path" class="w-32" alt=""></router-link>
+      </div> -->
+      <!-- <div class="w-1/3 flex flex-row justify-start gap-y-4">
+          <img :src="`https://image.tmdb.org/t/p/w500/${movie_actor[1].profile_path}`" class="w-32" alt="">
+          {{movie['actors'][1].korean_name}}
+          <img :src="`https://image.tmdb.org/t/p/w500/${movie['actors'][2].profile_path}`" class="w-32" alt="">
+          {{movie['actors'][2].korean_name}}
+            
+            
+          <img :src="`https://image.tmdb.org/t/p/w500/${movie['actors'][3].profile_path}`" class="w-32" alt="">
+          {{movie['actors'][3].korean_name}}
+      </div>
+      <div :v-for="item in 5">
+        <img :src="`https://image.tmdb.org/t/p/w500/${movie['actors'][item].profile_path}`" class="w-32" alt="">
+          {{movie['actors'][item].korean_name}}
+      </div> -->
+      </div>
+    </div>   
+
   <!-- </template> -->
-    </div>
+
 </template>
   
 
 
 <script>
-import {fetchMovie} from '@/api/index.js'
+// import {fetchMovie} from '@/api/index.js'
 export default {
   name: 'MovieDetail',
   components: {
@@ -29,26 +57,25 @@ export default {
   },
 
   },
-  data() {
-    return {
-      // dialog: false,
-      // notifications: false,
-      // sound: true,
-      // widgets: false,
-    }
-  },
+  
   computed: {
-    
+    actors(){
+      const tmp = this.movie.actors
+      // console.log(tmp)
+      return tmp
+    },
+    movie_actor(){
+    const {korean_name,profile_path, movies } = this.movie['actors']
+    // console.log(this.movie['actors'].length())
+    const actor = {korean_name,profile_path,movies}
+    actor.profile_path = `https://image.tmdb.org/t/p/w500/${actor.profile_path}`
+
+    return actor
+  }
   },
   async created() {
-    try {
-    const movie = this.movie.pk
-    const movieDetail = await fetchMovie(movie)
-    this.moviePK = movieDetail.data
-    console.log(this.moviePK)
-  } catch (error) {
-    console.error(error)
-  }
+    // const movie_act = this.movie['actors']
+    // console.log(movie_act.length)
   },
 }
 </script>
